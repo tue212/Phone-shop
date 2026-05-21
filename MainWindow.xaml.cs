19f77148b7e6187b77e2747932e395d24a727f4c
tue212 <phamxuantritue93@gmail.com>
2026-05-21 17:42:05 +0700

    phoneShop1st

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace PhoneShop
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Activated(object sender, WindowActivatedEventArgs args)
+        {
+            root.Navigate(typeof(ProductsPage));
+        }
+
+        private void navigator_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            var item = args.SelectedItem as NavigationViewItem;
+            string tag = item.Tag.ToString();
+
+            root.Navigate(tag switch
+            {
+                "ProductsPage" => typeof(ProductsPage),
+                "OrdersPage" => typeof(OrdersPage),
+                "ReportPage" => typeof(ReportPage),
+                _ => typeof(ProductsPage),
+            });
+        }
+    }
+}
---
+++ Password.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneShop
+{
+    public class Password
+    {
+        public static (string, string) Encrypt (string rawPassword)
+        {
+            byte[] passwordInBytes = Encoding.UTF8.GetBytes(rawPassword);
+            byte[] entropy = new byte[20];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(entropy);
+            }
+            byte[] encryptedPassword = ProtectedData.Protect(passwordInBytes, entropy, DataProtectionScope.CurrentUser);
+            return (Convert.ToBase64String(encryptedPassword), Convert.ToBase64String(entropy));
+        }
+        public static string Decrypt (string encryptedPassword, string entropy)
+        {
+            byte[] encryptedPasswordInBytes = Convert.FromBase64String(encryptedPassword);
+            byte[] entropyInBytes = Convert.FromBase64String(entropy);
+            byte[] decryptedPassword = ProtectedData.Unprotect(encryptedPasswordInBytes, entropyInBytes, DataProtectionScope.CurrentUser);
+            return Encoding.UTF8.GetString(decryptedPassword);
+        }
+    }
+}
